2da8a51a4ffa87d912102030e4ebdf86127a8db3
Cortney Thomas <thomas.cortney@outlook.com>
2018-12-04 03:03:59 -0500

    checkpoint 4 completed

---
+++ src/db/queries.topics.js
@@ -0,0 +1,12 @@
+const Topic = require('./models').Topic;
+
+module.exports = {
+
+    getAllTopics(callback) {
+        return Topic.all().then((topics) => {
+            callback(null, topics);
+        }).catch((err) => {
+            callback(err);
+        })
+    }
+}
